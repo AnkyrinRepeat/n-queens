2d7252aba707dbbae80f27bb0c56b92e012625a1
MKS-students <mks.students@hackreactor.com>
2015-06-17 14:39:25 -0500

    Passes all Board Conflicts

--- src/Board.js
+++ src/Board.js
@@ -79,11 +79,35 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
+      //iterate through array at rowIndex blah
+        //counter++ if there is a piece
+      //if counter is bigger than 1 return true
+      //else return false
+      var row = this.get(rowIndex);
+      var counter = 0;
+      for(var i = 0; i < row.length; i++) {
+        if(row[i] === 1) {
+          counter++;
+        }
+      }
+      if(counter > 1) {
+        return true;
+      }
+
       return false; // fixme
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
+      //for loop through rows
+        //pass to hasRowConflictAt
+        //if hasRowConflictAt is true, return true
+      var length = this.get(0).length
+      for(var i = 0; i < length; i++) {
+        if(this.hasRowConflictAt(i)) {
+          return true;
+        }
+      }
       return false; // fixme
     },
 
@@ -94,12 +118,36 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
+      //loop through every row and grab row[colIndex]
+        //IF board.Get(i)[colIndex] is one, increment counter
+      //If our counter is bigger than 1, return true, we found a conflict. Woo!
+      var length = this.get(0).length
+      var counter = 0;
+      for(var i = 0; i < length; i++) {
+        if(this.get(i)[colIndex] === 1) {
+          counter++;
+        }
+      }
+      if (counter > 1) {
+        return true;
+      }
       return false; // fixme
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
+      //for loop through rows
+        //pass to hasColConflictAt
+        //if hasColConflictAt is true, return true
+      var length = this.get(0).length
+      for(var i = 0; i < length; i++) {
+        if(this.hasColConflictAt(i)) {
+          return true;
+        }
+      }
+
       return false; // fixme
+
     },
 
 
@@ -109,12 +157,33 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var column = majorDiagonalColumnIndexAtFirstRow;
+      var length = this.get(0).length;
+      var counter = 0;
+      for(var i = 0; i < length; i++) {
+        //check if space is actually on board
+        if(column+i >= 0 && column+i < length) {
+          //check space for piece
+          if(this.get(i)[column+i]) {
+            counter++;
+          }
+        }
+      }
+      if(counter > 1) {
+        return true;
+      }
+      return false;
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      var length = this.get(0).length
+      for(var i = -length+1; i < length; i++) {
+        if(this.hasMajorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
@@ -124,12 +193,32 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var length = this.get(0).length;
+      var counter = 0;
+      var column = minorDiagonalColumnIndexAtFirstRow;
+
+      for (var i = 0; i < length; i++) {
+        if(column+i >= 0 && column+i < length) {
+          if (this.get(length-(i+1))[column+i]) {
+            counter++;
+          }
+        }
+      }
+      if(counter > 1) {
+        return true;
+      }
+      return false;
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      var length = this.get(0).length
+      for(var i = -length+1; i < length; i++) {
+        if(this.hasMinorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
